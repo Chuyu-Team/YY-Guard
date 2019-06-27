594b8e9070e7c24f5c7e6c3584ec953860ad80dd
mingkuang <mingkuang@live.com>
2019-06-27 12:54:51 +0800

    将__pfnDliNotifyHook2、__pfnDliFailureHook2调整为weak（感谢 CC）。

--- src/Build.cmd
+++ src/Build.cmd
@@ -17,21 +17,28 @@ goto:eof
 :BuildObj
 cl /O1 /Os /Oi /GS- /arch:IA32 /Z7 /MT /Fo"objs\\%Platform%\\%1" /Zl /c /D "NDEBUG" /D "__YY_GUARD_MIN_SUPPORT=%2" "%~dp0delayhlp.cpp"
 
+LibMaker.exe FixObj "%~dp0..\\objs\\%Platform%\\%1" /WeakExternFix:__pfnDliNotifyHook2=%PointType%
+LibMaker.exe FixObj "%~dp0..\\objs\\%Platform%\\%1" /WeakExternFix:__pfnDliFailureHook2=%PointType%
+
 goto:eof
 
 :Buildx86
+set PointType=4
 call:BuildObj YY_Guard.obj NTDDI_WINXP
 goto:eof
 
 
 :Buildx64
+set PointType=8
 call:BuildObj YY_Guard.obj NTDDI_WS03SP1
 goto:eof
 
 :Buildarm
+set PointType=4
 call:BuildObj YY_Guard.obj NTDDI_WIN8
 goto:eof
 
 :Buildarm64
+set PointType=8
 call:BuildObj YY_Guard.obj NTDDI_WIN10_RS3
 goto:eof
