ec46b81a8ca045ffc9b34b43fcaa74c85a329273
mingkuang <mingkuang@live.com>
2020-08-13 17:37:08 +0800

    更新编译脚本，使其更加通用化。

--- src/BuildAll.cmd
+++ src/BuildAll.cmd
@@ -1,27 +1,34 @@
 @echo off
 
 
+set BuiltInVsWhereExe="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not defined ProgramFiles(x86) ( set "BuiltInVsWhereExe="%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"" )
+
+if not exist %BuiltInVsWhereExe% (echo 请安装VS2017或者更高版本然后继续！& goto:eof )
+
+for /f "tokens=*" %%i in ('%BuiltInVsWhereExe% -latest -prerelease -property installationPath') do ( set LatestVisualStudioRoot=%%i)
+
+
+
 setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
+call "%LatestVisualStudioRoot%\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
 call "%~dp0Build.cmd"
 endlocal
 
 
 setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "%LatestVisualStudioRoot%\VC\Auxiliary\Build\vcvars64.bat"
 call "%~dp0Build.cmd"
 endlocal
 
 
 setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_arm.bat"
+call "%LatestVisualStudioRoot%\VC\Auxiliary\Build\vcvarsamd64_arm.bat"
 call "%~dp0Build.cmd"
 endlocal
 
 
 setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsamd64_arm64.bat"
+call "%LatestVisualStudioRoot%\VC\Auxiliary\Build\vcvarsamd64_arm64.bat"
 call "%~dp0Build.cmd"
 endlocal
-
-pause
